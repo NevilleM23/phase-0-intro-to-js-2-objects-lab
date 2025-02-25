8934a422f180804365ec33bc05b4baa40fd95ce1
NevilleMuli <neville.muli@gmail.com>
2025-02-25 15:48:12 +0300

    creates objects in index

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+const employee  = {
+    name: "Jeffrey", 
+    streetAddress:"15 Runner Ave"
+}
+function  updateEmployeeWithKeyAndValue(employee){
+    const newEmployee = {...employee}
+    newEmployee.name = "Sam"
+    newEmployee.streetAddress = "11 Broadway"
+    return newEmployee
+} 
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.name = "Sam"
+    employee.streetAddress = "12 Broadway"
+    return employee
+} 
+
+function deleteFromEmployeeByKey(employee){
+    const newEmployee = {...employee}
+    delete newEmployee.name 
+    return newEmployee
+} 
+
+function destructivelyDeleteFromEmployeeByKey(){
+    delete employee.name
+    return employee
+}
